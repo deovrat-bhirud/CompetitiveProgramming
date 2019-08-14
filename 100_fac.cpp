d821645703f2154aba88fe9791215aac01d157d5
Deovrat Bhirud <53523845+deovrat-bhirud@users.noreply.github.com>
2019-08-14 12:11:29 +0530

    Create 100_fac.cpp

---
+++ 100_fac.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#include <boost/multiprecision/cpp_int.hpp> 
+using namespace boost::multiprecision; 
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+        int k;
+        cin>>k;
+        int1024_t count=1;
+        
+        for(int i=1; i<=k;i++)
+            count=( int1024_t)count*i;
+        cout<<count<<endl;
+        count=1;
+    }
+    return 0;
+}
